2ed72ad6d249d65481e6868635ba184a378cb76e
Adam Brill <brill.adam@gmail.com>
2013-02-01 13:30:50 +0000

    Initial Commit For Rails Tutorial Sample App

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ruby On Rails Tutorial: demo application
+
+This is the demo app for Ruby on Rails Tutorial by Michael Hartl
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '568a510ae918dd97576eb47c1b04d12e51a4d348d7fe10ce7c6de3d90df3d8a97e3467b97ff0183b71bf8a0a6b31037007c082bf6f5de22ed5669078ecb8a515'
